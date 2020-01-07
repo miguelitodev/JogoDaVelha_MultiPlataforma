d8d680f664c19f8ee756b2907e15181b74fa84be
miguelrisquelme <miguelnascimento22@hotmail.com>
2020-01-07 13:44:56 -0300

    Projeto completo

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Jogo_da_velha_multi-plataforma
+# Jogo da velha multi plataforma
+<br/>
 Esse é um projeto pessoal, no qual eu desenvolvi um joguinho da forca, responsivo, ou seja funciona tanto no computador como nos smartphones e tablets, as tecnologias utilizadas foram apenas HTML5, CSS3 e JS, no qual para deixar responsivo eu utilizei flexbox que é próprio do CSS.
+
+# Prints ilustrativos
+
+<img src="./src/img/print-1.png">
+<p>Esta imagem representa o usuário ao abrir no computador</p>
+
+<br>
+<br>
+
+<img src="./src/img/print-2.png">
+<p>Esta imagem representa o usuário ao abrir no smartphone</p>
+
---
+++ app/js/script.js
@@ -0,0 +1,130 @@
+const jogador1 = "X";
+const jogador2 = "O";
+var vez = jogador1;
+var gameOver = false;
+
+atualizaVez();
+inicializarEspacos();
+
+function atualizaVez() {
+
+    if(gameOver){
+        return;
+    }
+
+    if(vez == jogador1){
+        var player = document.querySelectorAll("div#vez img")[0];
+        player.setAttribute("src", "img/X.png");
+    } else {
+        var player = document.querySelectorAll("div#vez img")[0];
+        player.setAttribute("src", "img/O.png");
+    }
+
+}
+
+function inicializarEspacos() {
+    var espacos = document.getElementsByClassName('espacos');
+    for(var i = 0; i < espacos.length; i++){
+        espacos[i].addEventListener('click', 
+            function() {
+                
+                if(gameOver){
+                    return
+                }
+
+                if(this.getElementsByTagName('img').length == 0){
+
+                    if(vez == jogador1){
+                        this.innerHTML = "<img id='imgJogada' src='img/X.png'/>";
+                        this.setAttribute("jogada", jogador1);
+                        vez = jogador2;
+                    } else {
+                        this.innerHTML = "<img id='imgJogada' src='img/O.png'/>";
+                        this.setAttribute("jogada", jogador2);
+                        vez = jogador1;
+                    }
+                    atualizaVez();
+                    verificarVencedor();
+                }
+            }
+        );
+    }
+}
+
+
+async function verificarVencedor() {
+    var a1 = document.getElementById('a1').getAttribute('jogada');
+    var a2 = document.getElementById('a2').getAttribute('jogada');
+    var a3 = document.getElementById('a3').getAttribute('jogada');
+    
+    var b1 = document.getElementById('b1').getAttribute('jogada');
+    var b2 = document.getElementById('b2').getAttribute('jogada');
+    var b3 = document.getElementById('b3').getAttribute('jogada');
+    
+    var c1 = document.getElementById('c1').getAttribute('jogada');
+    var c2 = document.getElementById('c2').getAttribute('jogada');
+    var c3 = document.getElementById('c3').getAttribute('jogada');
+
+    var vencedor = "";
+
+    if((a1 == b1 && a1 == c1 && a1 != "") || (a1 == a2 & a1 == a3 && a1 != "") || (a1 == a2 && a1 == c3 && a1 != "")){
+        vencedor = a1;
+    } else if((b2 == b1 && b2 == b3 && b2 != "") || (b2 == a2 && b2 == c2 && b2 != "") || (b2 == a3 && b2 == c1 && b2 != "")){
+        vencedor = b2;
+    } else if(((c3 == c2 && c3 == c1) || (c3 == a3 && c3 == b3)) && c3 != ""){
+        vencedor = c3;
+    }
+
+    if (vencedor != ""){
+        gameOver = true;
+
+        await sleep(50);
+
+        if(vencedor == "X"){
+            modalBgJogo.classList.add('bg-active-jogo');
+            var player = document.querySelectorAll("div#modalJogo img")[0];
+            player.setAttribute("src", "img/X.png");
+            modalCloseJogo.addEventListener('click', function(){
+                modalBgJogo.classList.remove('bg-active-jogo');
+            });
+        } else {
+            modalBgJogo.classList.add('bg-active-jogo');
+            var player = document.querySelectorAll("div#modalJogo img")[0];
+            player.setAttribute("src", "img/O.png");
+            modalCloseJogo.addEventListener('click', function(){
+                modalBgJogo.classList.remove('bg-active-jogo');
+            });
+        }
+        
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/* BOTÃO REINICIAR*/
+
+function reiniciar() {
+    window.location.reload();
+}
+
+
+/* MODAL CONTATO*/
+
+var modalBtn = document.querySelector('.modal-btn');
+var modalBg = document.querySelector('.modal-bg');
+var modalClose = document.querySelector('.modal-close');
+
+modalBtn.addEventListener('click', function(e){
+    modalBg.classList.add('bg-active'); 
+     e.preventDefault();
+});
+
+modalClose.addEventListener('click', function(){
+    modalBg.classList.remove('bg-active');
+});
+
+/* MODAL JOGO */
+var modalBgJogo = document.querySelector('.modal-bg-jogo');
+var modalCloseJogo = document.querySelector('.modal-close-jogo');
